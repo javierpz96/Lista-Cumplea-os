a8f9ebe1f3cf530b3faec88ed0625daf395365c6
Javier <75548156+javierpz96@users.noreply.github.com>
2021-12-27 15:20:30 -0300

    primer commit

---
+++ setup/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+function App() {
+  const [people,setPeople] =useState(data)
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Cumpleaños hoy</h3>
+        <List people={people}/>
+        <button onClick={() => setPeople([])}>
+          Limpiar todo
+        </button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
